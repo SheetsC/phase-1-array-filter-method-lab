b9f46ca885e5bdc65d254337ee74c872938da318
Connor Sheets <connorsheets32@gmail.com>
2023-01-12 13:04:09 -0600

    filter out the bad stuff

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+// Code your solution here()
+// console.log(drivers)
+
+    //}     
+function findMatching(array, srch){
+    return array.filter((driver) => driver.toUpperCase() == srch.toUpperCase());
+}
+// const findMatching = drivers.filter(driver=> driver == 'Bobby');
+function fuzzyMatch(array, srch){
+    return array.filter((driver) => driver[0] == srch[0]);
+}
+function matchName(index, srch){
+    return index.filter((driver) => driver.name == srch );
+}
